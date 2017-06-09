fe80504dd67fdba63052ee8a4368f432e0c70c7f
Jyothi Kiran <j.kiran@elsevier.com>
2017-06-09 15:06:20 -0400

    removed jquery and used angular scope to fetch form values

--- src/main/webapp/static/js/app/login/register.js
+++ src/main/webapp/static/js/app/login/register.js
@@ -1,15 +1,10 @@
 wtApp.controller('registrationController', [ '$scope', '$http', function($scope, $http) {
 	$scope.register = function() {
-		var workerName = $("#profName").val();
-		var mobileNum = $("#mobileNum").val();
-		var username = $("#username").val();
-		var password = $("#password").val();
-		alert(workerName + mobileNum+username+password)
 		var loginDetailsForm =  {  
-									phoneNumber : mobileNum, 
-									password : password,
-									userName : username, 
-									workerName : workerName 
+									phoneNumber : $scope.user.mobileNum, 
+									password : $scope.user.password,
+									userName :  $scope.user.userName, 
+									workerName : $scope.user.profileName 
 								};
 		$http.post('/registerUser',loginDetailsForm,{headers:{'Accept': 'application/json','Content-Type': 'application/json'}}).success(function(response){
 				window.location = "/login.uri?firstLogin=true";
